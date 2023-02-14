c663ff930226d9de1871c08fc3f28579f9979a2b
Evannfries <evann.fries@yahoo.com>
2023-02-14 13:48:14 -0600

    homepage style changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,20 @@ import "../styles/Home.css";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import EmailIcon from "@material-ui/icons/Email";
+import 'animate.css'
 // import { Link } from 'react-router-dom';
 
 function Home() {
   return (
     <div className='home'>
       <div className='about'>
-        <h2>FUN In The Front-End</h2>
+        <h2><span className='fun'>Fun</span> In The Front-End</h2>
         <h2>Business In The Back</h2>
         <div className='prompt'> 
           <p>
           My name is Evann, and I'm aspiring software developer looking to create or improve your web apps!
           </p>
+          <div className='icons'>
           <a href='https://www.linkedin.com/in/evann-fries/'>
           <LinkedInIcon/>
           </a>
@@ -24,26 +26,9 @@ function Home() {
           <a>
           <EmailIcon />
           </a>
+          </div>
         </div>
       </div> 
-      {/* <div className='skills'> 
-        <h1>Skills</h1> 
-        <ol className='list'>
-          <li className='item'>
-            <h2> Languages </h2>
-            <span> JavaScript, Python, HTML, CSS, EJS, Typescript </span>
-          </li>
-          <li className='item'>
-            <h2> Libraries and Frameworks </h2>
-            <span> React, Express.js, Mongoose, Flask </span>
-          </li>
-          <li className='item'>
-            <h2> Databases/Other </h2>
-            <span> MongoDB, Strapi, MySQL, PostgreSQL, Git, Postman, Debugging </span>
-          </li>
-        </ol>
-      </div> */}
-      
     </div>
   )
 }
--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import '../styles/Skills.css'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <div className='skills'> 
+        <h1>Skills</h1> 
+        <ol className='list'>
+          <li className='item'>
+            <h2> Languages </h2>
+            <span> JavaScript, Python, HTML, CSS, EJS, Typescript </span>
+          </li>
+          <li className='item'>
+            <h2> Libraries and Frameworks </h2>
+            <span> React, Express.js, Mongoose, Flask </span>
+          </li>
+          <li className='item'>
+            <h2> Databases/Other </h2>
+            <span> MongoDB, Strapi, MySQL, PostgreSQL, Git, Postman, Debugging </span>
+          </li>
+        </ol>
+      </div>
   )
 }
 
