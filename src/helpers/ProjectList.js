3b74a083ecc184663145bfca681d5ca3199fa9f7
Evannfries <evann.fries@yahoo.com>
2023-02-16 12:11:49 -0600

    project list updates

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -9,19 +9,27 @@ import GCI from '../assets/GCI-Database.png'
 export const projectList = [
     {
         name: "Success+Dress",
-        desc: "Mock E-Commerce Website",
+        desc: "Mock e-commerce website for business attire",
         image: SDHome,
         skill: "React, CSS, Strapi",
         link: "https://success-dress.net/",
         github: "https://github.com/evannf/shopping-app"
     },
     {
-        name: "Tamagotchi Project",
-        desc: "An app where you take care of a bunny!",
-        image: Proj1,
-        skill: "HTML, CSS, JavaScript",
-        link: "https://bunnigatchi.netlify.app/",
-        github: "https://github.com/evannf/tamagotchi-project-"
+        name: "GCI Villains Database",
+        desc: "A database platform cataloging villains from the show The Venture Bros",
+        image: GCI,
+        skill: "Express, EJS, JavaScript, CSS",
+        link: "https://gci-villains-database-ievf3.ondigitalocean.app/",
+        github: "https://github.com/evannf/GCI_Villain_Database"
+    },
+    {
+        name: "Social Meatia",
+        desc: "Mock social media app based on meat",
+        image: SocialMetia,
+        skill: "React, Express, CSS, JavaScript",
+        link: "",
+        github: "https://github.com/evannf/capstone_frontend"
     },
     {
         name: "Tug of Words game",
@@ -32,20 +40,12 @@ export const projectList = [
         github: "https://github.com/evannf/Tug-of-Words"
     },
     {
-        name: "GCI Villains Database",
-        desc: "A database platform cataloging the villains from the show The Venture Bros",
-        image: GCI,
-        skill: "Express, EJS, JavaScript, CSS",
-        link: "https://gci-villains-database-ievf3.ondigitalocean.app/",
-        github: "https://github.com/evannf/GCI_Villain_Database"
-    },
-    {
-        name: "Social Meatia",
-        desc: "Mock social media app (not currently deployed)",
-        image: SocialMetia,
-        skill: "React, Express, CSS, JavaScript",
-        link: "",
-        github: "https://github.com/evannf/capstone_frontend"
-    },
+        name: "Tamagotchi Project",
+        desc: "An app where you take care of a bunny!",
+        image: Proj1,
+        skill: "HTML, CSS, JavaScript",
+        link: "https://bunnigatchi.netlify.app/",
+        github: "https://github.com/evannf/tamagotchi-project-"
+    }
 
 ]
